038a7fa15ff4d2f26af5589f9917300af34d67bd
Luna <100821413+SillyCiely@users.noreply.github.com>
2024-11-19 22:43:18 +1100

    updated comments for less visual clutter

--- backend/routes/blogposts-route.js
+++ backend/routes/blogposts-route.js
@@ -18,38 +18,23 @@ const {
     populateTestData,
 } = require('../controllers/blogposts-controller')
 
-/* ----------------------------------- */
-/*             GET REQUESTS            */
-/* ----------------------------------- */
-
+// GET REQUESTS
 router.get('/posts', getAllPosts)
 router.get('/posts/:postId', getPostById)
 router.get('/posts/author/:authorId', getPostsByAuthor)
 router.get('/posts/category/:category', getPostsByCategory)
 
-/* ----------------------------------- */
-/*            POST REQUESTS            */
-/* ----------------------------------- */
-
+// POST REQUESTS
 router.post('/posts/new', authenticateUser, upload.single('image'), createPost)
 
-/* ----------------------------------- */
-/*             PUT REQUESTS            */
-/* ----------------------------------- */
-
+// PUT REQUESTS
 router.put('/posts/:postId/update', authenticateUser, updatePost)
 router.put('/posts/:postId/like', authenticateUser, likePost)
 
-/* ----------------------------------- */
-/*            DELETE REQUESTS          */
-/* ----------------------------------- */
-
+// DELETE REQUESTS
 router.delete('/posts/:postId/delete', authenticateUser, deletePost)
 
-/* ----------------------------------- */
-/*          POPULATE TEST DATA         */
-/* ----------------------------------- */
-
+// POPULATE TEST DATA
 router.post('/posts/populate-test-data', populateTestData)
 
 module.exports = router
