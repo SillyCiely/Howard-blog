fb10b7f6ee0207440fa3d24ba74d429a32ae3681
Luna <100821413+SillyCiely@users.noreply.github.com>
2024-11-21 00:57:22 +1100

    added some more functions

--- backend/controllers/comments-controller.js
+++ backend/controllers/comments-controller.js
@@ -2,12 +2,16 @@ const Comment = require('../models/comments-schema')
 const { Users } = require('../models/users-schema')
 const commentsTestData = require('../models/test-data/comments-testdata')
 var { generateID } = require('../models/users-model')
+const Post = require("../models/blogposts-schema");
 
 const getCommentById = async (req, res) => {
     try {
-        const comment = await Comment.findOne({ cid: req.params.cid })
+        const comment = await Comment.findOne({ commentId: req.params.commentId })
+
         if (comment) {
-            res.json(comment)
+            const commenter = await Users.findOne({ userId: comment.commenterId })
+            const commenterName = commenter.username
+            res.json({ comment: comment, commenter: commenterName })
         } else {
             res.status(404).send('Comment not found')
         }
@@ -20,10 +24,11 @@ const getCommentById = async (req, res) => {
 const getCommentsByPostId = async (req, res) => {
     try {
         const comments = await Comment.find({ postId: req.params.postId })
+
         if (comments.length > 0) {
             res.json(comments)
         } else {
-            res.status(404).send('No comments found for this post')
+            res.status(404).send('This post does not have any comments yet. Add one!')
         }
     } catch (error) {
         console.error('Error fetching comments: ', error)
@@ -43,18 +48,19 @@ const getCommentsByUserId = async (req, res) => {
 
 const createComment = async (req, res) => {
     try {
-        const { body } = req.body
-        const cid = generateID()
-        const userId = req.user.id
-
         const newComment = await Comment.create({
-            cid,
-            userId,
-            body,
-            createdAt: new Date(),
-            commentImage: req.file ? req.file.buffer : undefined,
+            commentId: generateID(),
+            postID: req.body.postId,
+            commenterId: req.user.userId,
+            commentBody: req.body.commentBody,
+            // comments do not support images
+            // todo: potential for image attachments?
+            //  needs schema update...
+            // image: req.file.buffer,
+            // imageMimeType: req.file.mimetype,
+            likes: 0,
+            usersLikes: [],
         })
-
         res.status(201).json(newComment)
     } catch (error) {
         console.error('Error creating comment: ', error)
@@ -64,43 +70,58 @@ const createComment = async (req, res) => {
 
 const updateComment = async (req, res) => {
     try {
-        const { commentID } = req.params
-        const { body } = req.body
+        const comment = await Comment.findOne({ commentID })
 
-        const comment = await Comment.findOne({ cid: commentID })
         if (!comment) {
             return res.status(404).send('Comment not found')
         }
-        if (comment.userId !== req.user.id) {
-            return res.status(403).send('You do not have permission to update this comment')
+
+        if (comment.commenterId === req.user.id) {
+            await Comment.updateOne({ commentId: req.params.commentId }, { $set: { body: req.body.body } })
+            res.status(200).send('Comment updated successfully')
+        } else {
+            return res.status(403).send('Unauthorized')
         }
-
-        comment.body = body
-        await comment.save()
-
-        res.status(200).json(comment)
     } catch (error) {
         console.error('Error updating comment: ', error)
         res.status(500).send('Error updating comment')
     }
 }
 
+const likeComment = async (req, res) => {
+    try {
+        const comment = await Comment.findOne({commentId: req.params.commentId})
+        if (!comment.userLikes.includes(req.user.userId)) {
+            comment.likes += 1
+            comment.usersLikes.push(req.user.userId)
+            await comment.save()
+            res.status(200).send('Comment liked')
+        } else {
+            res.status(403).send('Already liked')
+        }
+    } catch (error) {
+        console.error('Error liking comment: ', error)
+        res.status(500).send('Error')
+    }
+}
+
 const deleteComment = async (req, res) => {
     try {
-        const comment = await Comment.findOne({ cid: req.params.cid })
+        const comment = await Comment.findOne({ commentId: req.params.commentId })
+
         if (!comment) {
             return res.status(404).send('Comment not found')
         }
-        if (comment.userId !== req.user.id) {
-            return res
-                .status(403)
-                .send('You do not have permission to delete this comment')
+
+        if (comment.commenterId === req.user.userId) {
+            await Comment.deleteOne({ commentId: req.params.commentId })
+            res.status(200).send('Comment deleted successfully')
+        } else {
+            res.status(403).send('Unauthorized')
         }
-        await Comment.deleteOne({ cid: req.params.cid })
-        res.status(200).send('Comment deleted successfully')
     } catch (error) {
-        console.error('Error deleting comment: ', error)
-        res.status(500).send('Error deleting comment')
+        console.error('Error deleting comment', error)
+        res.status(500).send('Error')
     }
 }
 
