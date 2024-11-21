40555fa404927ac4b3f58ca85ddf0346ba13a284
Luna <100821413+SillyCiely@users.noreply.github.com>
2024-11-21 12:30:33 +1100

    authenticateUser import corrected

--- backend/routes/comments-route.js
+++ backend/routes/comments-route.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
+
+const authenticateUser = require('../middleware/authenticateUser')
+// const multer = require('multer')
+// const storage = multer.memoryStorage()
+// const upload = multer({ storage: storage })
+
 const {
     getCommentById,
     getCommentsByPostId,
@@ -10,25 +16,21 @@ const {
     deleteComment,
     populateTestData
 } = require('../controllers/comments-controller')
-const { authenticateUser } = require('../middleware/authenticateUser')
-const multer = require('multer')
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
 
 // GET REQUESTS
 router.get('/comments/:commentID', getCommentById)
 router.get('/posts/:postId/comments', getCommentsByPostId)
 router.get('/users/:userId/comments', getCommentsByUserId)
 
-// // POST REQUESTS
-// router.post('/comments/new', authenticateUser, upload.single('commentImage'), createComment)
-// router.post('/comments/:commentID/like', authenticateUser, likeComment)
-//
-// // PUT REQUESTS
-// router.put('/comments/:commentID/update', authenticateUser, updateComment)
-//
-// // DELETE REQUESTS
-// router.delete('/comments/:commentID/delete', authenticateUser, deleteComment)
+// POST REQUESTS
+router.post('/comments/new', authenticateUser, createComment)
+router.post('/comments/:commentID/like', authenticateUser, likeComment)
+
+// PUT REQUESTS
+router.put('/comments/:commentID/update', authenticateUser, updateComment)
+
+// DELETE REQUESTS
+router.delete('/comments/:commentID/delete', authenticateUser, deleteComment)
 
 // POPULATE TEST DATA
 router.post('/comments/populate-test-data', populateTestData)
