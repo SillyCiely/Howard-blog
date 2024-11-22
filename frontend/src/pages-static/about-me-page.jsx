9190a3366635266a306875eafaedcc97a323625d
Luna <100821413+SillyCiely@users.noreply.github.com>
2024-11-23 02:20:58 +1100

    created about me page like https://www.drfan.com.au/

---
+++ frontend/src/pages-static/about-me-page.jsx
@@ -0,0 +1,13 @@
+import PageTemplate from "../components/page-template";
+
+const AboutMePage = () => {
+    return (
+        <PageTemplate title={`About Me`}>
+            <h2>Dr Howard Fan</h2>
+            <h3>BMed, MD, MHM, FRACS</h3>
+            <p>Dr Howard Fan graduated from the University of New South Wales (UNSW) with a Bachelor of Medical Studies, Doctor of Medicine, and Masters of Health Management. Dr Fan subsequently worked with New South Wales Health before completing fellowship in general surgery to become a Fellow of the Royal Australasian College of Surgery (FRACS). His special interests are in gastrointestinal (GI) surgery focusing on minimally invasive, laparoscopic, and endoscopic techniques. </p>
+        </PageTemplate>
+    )
+}
+
+export default AboutMePage
