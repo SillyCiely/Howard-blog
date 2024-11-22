a6e9a9d5d29978dd5645cc24dda7223dd49413a9
Luna <100821413+SillyCiely@users.noreply.github.com>
2024-11-23 03:16:45 +1100

    added image and styled into box-card (temp)

--- frontend/src/pages-static/about-me-page.jsx
+++ frontend/src/pages-static/about-me-page.jsx
@@ -1,11 +1,25 @@
 import PageTemplate from "../components/page-template";
+import ContentContainer from "../components/content-container";
+import howardImage from '../media/howard-20230114.jpg';
+import '../styles/image-styling.css';
 
 const AboutMePage = () => {
     return (
         <PageTemplate title={`About Me`}>
-            <h2>Dr Howard Fan</h2>
-            <h3>BMed, MD, MHM, FRACS</h3>
-            <p>Dr Howard Fan graduated from the University of New South Wales (UNSW) with a Bachelor of Medical Studies, Doctor of Medicine, and Masters of Health Management. Dr Fan subsequently worked with New South Wales Health before completing fellowship in general surgery to become a Fellow of the Royal Australasian College of Surgery (FRACS). His special interests are in gastrointestinal (GI) surgery focusing on minimally invasive, laparoscopic, and endoscopic techniques. </p>
+            <ContentContainer className={`flex-row-container box-container`}>
+                <img src={howardImage} alt={`Dr Howard Fan`} id={`howardImage`}/>
+                <div id={`about-me-text`}>
+                    <h2>Dr Howard Fan</h2>
+                    <h3>BMed, MD, MHM, FRACS</h3>
+                    <p>Dr Howard Fan graduated from the University of New South Wales (UNSW) with a Bachelor of Medical
+                        Studies, Doctor of Medicine, and Masters of Health Management. He subsequently worked with
+                        New South Wales Health before completing fellowship in general surgery to become a Fellow of the
+                        Royal Australasian College of Surgery (FRACS).
+                        <br/><br/>
+                        Dr Fan's special interests are in gastrointestinal (GI)
+                        surgery focusing on minimally invasive, laparoscopic, and endoscopic techniques. </p>
+                </div>
+            </ContentContainer>
         </PageTemplate>
     )
 }
