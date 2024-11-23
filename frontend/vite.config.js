1aa0bd4d6a2d4ff849f8c08bf0cbdf20762a5ebe
Luna <100821413+SillyCiely@users.noreply.github.com>
2024-11-23 15:02:26 +1100

    can pull blog info from cloud database mongdb

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
+dotenv.config()
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: process.env.HOST ?? 'localhost',
-    port: process.env.PORT ?? '5173'
-  },
-  define: {
-    'import.meta.env.server_url': process.env.SERVER_URL ?? '"http://localhost:3000/"'
-  }
+    plugins: [react()],
+    server: {
+        host: process.env.HOST ?? 'localhost',
+        port: process.env.PORT ?? '5173'
+    },
+    define: {
+        'import.meta.env.VITE_SERVER_URL':
+            JSON.stringify(process.env.SERVER_URL ?? 'http://localhost:3000'),
+        'process.env': process.env
+    }
 })
